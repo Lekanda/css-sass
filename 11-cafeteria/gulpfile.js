4b5c3683a2722e34fa57539f8e30753cdd4251ae
lekanda <abernaolao@gmail.com>
2022-01-02 18:17:49 +0100

    Compilar SASS con Gulp.

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,8 +1,14 @@
+const {src, dest} = require('gulp');
+// De gulp-sass traemos el compilador.
+const sass = require('gulp-sass')(require('sass'));
 
-
-function tarea(done){
-    console.log('Hola mundo');
+function css(done){
+    src('src/scss/app.scss')
+        // Compilar
+        .pipe(sass())
+        /// Destino de la compilacion
+        .pipe(dest('./build/css'))
     done();
 }
 
-exports.tarea = tarea;
+exports.css = css;
