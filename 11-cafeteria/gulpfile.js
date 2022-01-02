bad18883cf379bfd6f3e869e3b197c3ba0279ea3
lekanda <abernaolao@gmail.com>
2022-01-02 19:17:25 +0100

    Autoprefixer.

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,11 +1,14 @@
 const {src, dest,watch} = require('gulp');
 // De gulp-sass traemos el compilador.
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done){
     src('src/scss/app.scss')
         // Compilar
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()])) // Crea codigo para navegadores viejos. Browserlist en package.json
         /// Destino de la compilacion
         .pipe(dest('./build/css'))
     done();
