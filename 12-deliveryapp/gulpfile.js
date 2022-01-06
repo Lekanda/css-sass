3ef626e9984f665700b8569afea5065f9461f0d1
lekanda <abernaolao@gmail.com>
2022-01-06 11:40:50 +0100

    Agregar codigo SASS al Header.

--- 12-deliveryapp/gulpfile.js
+++ 12-deliveryapp/gulpfile.js
@@ -16,7 +16,8 @@ function css( done ) {
     src('src/scss/app.scss')
         .pipe( sourcemaps.init() )
         .pipe( sass() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        // .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( postcss([ autoprefixer()]) )
         .pipe( sourcemaps.write('.'))
         .pipe( dest('build/css') )
 
