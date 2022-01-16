2eefc77e401a8fecb8b101419cc872b34dc02fda
lekanda <abernaolao@gmail.com>
2022-01-16 14:26:49 +0100

    Configurar el slider con mas configuraciones.

--- 16-carolinaspa/js/app.js
+++ 16-carolinaspa/js/app.js
@@ -1,5 +1,12 @@
 window.swiper = new Swiper({
     el:'.slider__contenedor',
     slideClass: 'slider__slide',
-    createElements: true
+    createElements: true,
+    autoplay: {
+        delay: 3000,
+    },
+    loop: true,
+    spaceBetween: 60,
+    pagination: true,
+    navigation: true
 });
