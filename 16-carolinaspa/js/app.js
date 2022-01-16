d99dfa66be1a07b5536425b6668a260fa7a2896a
lekanda <abernaolao@gmail.com>
2022-01-16 14:55:32 +0100

    Efecto cubo para imagenes con swipper.

--- 16-carolinaspa/js/app.js
+++ 16-carolinaspa/js/app.js
@@ -1,12 +1,32 @@
-window.swiper = new Swiper({
-    el:'.slider__contenedor',
-    slideClass: 'slider__slide',
-    createElements: true,
-    autoplay: {
-        delay: 3000,
+var swiper = new Swiper(".swiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
     },
-    loop: true,
-    spaceBetween: 60,
-    pagination: true,
-    navigation: true
+    pagination: {
+      el: ".swiper-pagination",
+    },
 });
+
+
+
+
+
+
+
+// window.swiper = new Swiper({
+//     el:'.slider__contenedor',
+//     slideClass: 'slider__slide',
+//     createElements: true,
+//     autoplay: {
+//         delay: 3000,
+//     },
+//     loop: true,
+//     spaceBetween: 60,
+//     pagination: true,
+//     navigation: true
+// });
